1d9a815c48749705a94c4ebb22f6466ac5c9db3a
Freek Lijten <freek.lijten@procurios.nl>
2011-10-07 16:12:20 +0200

    Use Consumer model for creating new Consumer

--- src/example/provider/create_consumer.php
+++ src/example/provider/create_consumer.php
@@ -2,32 +2,21 @@
 /**
  * @Author	Freek Lijten
  */
+require_once(__DIR__ . '/../../lib/AutoLoader.php');
+new AutoLoader();
 
-$DB = new mysqli('localhost', 'root', null, 'oauth');
-#TODO use consumer model
-#TODO smarter method for generating key and secret, generateToken for instance :/
-function newConsumerInfo($DB)
-{
-	$handle = fopen('/dev/urandom', 'rb');
-	$random = fread($handle, 200);
-	fclose($handle);
+//generate random keys
+$handle = fopen('/dev/urandom', 'rb');
+$random = fread($handle, 200);
+fclose($handle);
+$consumerKey = sha1(substr($random, 0, 100));
+$consumerSecret = sha1(substr($random, 100, 100));
 
-	$consumerKey = sha1(substr($random, 0, 100));
-	$consumerSecret = sha1(substr($random, 100, 100));
+//create consumer model
+$Consumer = new OAuthConsumerModel(Configuration::getDataStore());
+$Consumer->setConsumerCreateDate(time());
+$Consumer->setConsumerKey($consumerKey);
+$Consumer->setConsumerSecret($consumerSecret);
+$Consumer->save();
 
-	$sql = "INSERT INTO `oauth_provider_consumer`
-			SET consumer_key = '" . $DB->real_escape_string($consumerKey) . "',
-				consumer_secret = '" . $DB->real_escape_string($consumerSecret) . "',
-				consumer_create_date = '" . $DB->real_escape_string(time()) . "'";
-
-	if ($DB->query($sql)) {
-		return array('key' => $consumerKey, 'secret' => $consumerSecret);
-	}
-	return false;
-}
-
-
-
-echo '<pre>';
-var_dump(newConsumerInfo($DB));
-echo '</pre>';
+echo "Consumer key: $consumerKey <br />Consumer secret: $consumerSecret";
