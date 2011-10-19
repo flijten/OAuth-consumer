13f3817e2752726aa4d427bdb8936d3316eb4524
Freek Lijten <freek.lijten@procurios.nl>
2011-10-19 21:35:22 +0200

    Fixed the key and secret output

--- src/example/provider/create_consumer.php
+++ src/example/provider/create_consumer.php
@@ -10,6 +10,12 @@ $Consumer = new OAuthConsumerModel(Configuration::getDataStore());
 $Consumer->setConsumerCreateDate(time());
 $Consumer->setConsumerKey(OAuthProviderWrapper::generateToken());
 $Consumer->setConsumerSecret(OAuthProviderWrapper::generateToken());
-$Consumer->save();
 
-echo "Consumer key: $consumerKey <br />Consumer secret: $consumerSecret";
+try {
+	$Consumer->save();
+} catch (DataStoreCreateException $Exception) {
+	echo $Exception->getMessage();
+	exit;
+}
+
+echo "Consumer key: " . $Consumer->getConsumerKey() . "<br />Consumer secret: " . $Consumer->getConsumerSecret();
