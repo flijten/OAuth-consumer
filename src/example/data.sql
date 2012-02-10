ab4f60c27f1b97aa91fd39e87cbc05d71d560462
Freek Lijten <freek.lijten@procurios.nl>
2012-02-10 10:49:08 +0100

    Change texts of example user 2 to be distinct from user 1's texts

--- src/example/data.sql
+++ src/example/data.sql
@@ -9,8 +9,8 @@ INSERT INTO `user_messages` (`user_id`, `message_text`) VALUES
 ('1', 'Text for user 1 # 2'),
 ('1', 'Text for user 1 # 3'),
 ('1', 'Text for user 1 # 4'),
-('2', 'Text for user 1 # 1'),
-('2', 'Text for user 1 # 2');
+('2', 'Text for user 2 # 1'),
+('2', 'Text for user 2 # 2');
 
 INSERT INTO `user_images` (`user_id`, `image_url`) VALUES
 ('1', 'http://static.php.net/www.php.net/images/php.gif'),
